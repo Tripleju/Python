32f1ab39f4e57fc7ff2f6be35e99f64e5793ef76
July <filimonovajuly@gmail.com>
2022-10-27 16:08:19 +0300

    добавлены задания из лекции 3 и семинара 5

---
+++ Lessons/Seminar_5/HW_5.py
@@ -0,0 +1,15 @@
+
+
+#2
+#from itertools import groupby, starmap
+
+#from os import path   exists
+
+
+#3
+# Эмодзи
+# https://unicode-table.com/ru/emoji/
+
+#4
+# Стратегии выигрыша
+# https://informatika.shkolkovo.net/catalog/igry/prostejshie-igry-poisk-vyigryshnoj-strategii
