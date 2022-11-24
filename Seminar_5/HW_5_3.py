8287d9f301901adbacc6572437ccb4ef9703b339
July <filimonovajuly@gmail.com>
2022-11-24 14:51:50 +0300

    крестики-нолики базовая версия

--- Seminar_5/HW_5_3.py
+++ Seminar_5/HW_5_3.py
@@ -2,48 +2,69 @@
 # Эмодзи
 # https://unicode-table.com/ru/emoji/
 
-# подумаю об этом завтра. сегодня уже не могу.
-
 	
 # -*- coding: utf-8 -*-
 
-# board = range(1,10)
+board = list(range(1,10))
 
-# def draw_board(board):
-#     print ''
-#     for i in range(3):
-#         print "|", board[0+i*3], "|", board[1+i*3], "|", board[2+i*3], "|"
-#         print ''
+def draw_board(board):
+    print('-------------')
+    for i in range(3):
+        print("|", board[0+i*3], "|", board[1+i*3], "|", board[2+i*3], "|")
+        print('-------------')
 
-# draw_board(board)
+draw_board(board)
 
-# print(1)
+def step_maps(step,symbol):
+    ind = board.index(step)
+    board[ind] = symbol
 
-# Инициализация карты
-maps = [1,2,3,
-        4,5,6,
-        7,8,9]
- 
-# Инициализация победных линий
-victories = [[0,1,2],
-             [3,4,5],
-             [6,7,8],
-             [0,3,6],
-             [1,4,7],
-             [2,5,8],
-             [0,4,8],
-             [2,4,6]]
- 
-# Вывод карты на экран
-def print_maps():
-    print(maps[0], end = " ")
-    print(maps[1], end = " ")
-    print(maps[2])
- 
-    print(maps[3], end = " ")
-    print(maps[4], end = " ")
-    print(maps[5])
- 
-    print(maps[6], end = " ")
-    print(maps[7], end = " ")
-    print(maps[8])    
+def take_input(player_token):
+    valid = False
+    while not valid:
+        player_answer = input("Куда поставим " + player_token+"? ")
+        try:
+            player_answer = int(player_answer)
+        except:
+            print("Некорректный ввод. Вы уверены, что ввели число?")
+            continue
+        if player_answer >= 1 and player_answer <= 9:
+            if str(board[player_answer-1]).isdigit():
+                board[player_answer-1]=player_token
+                valid = True
+            else:
+                print("Эта клеточка уже занята")
+        else:
+            print("Некорректный ввод. Введите число от 1 до 9 чтобы походить.")
+
+def check_win(board):
+    win_coord = ((0,1,2),(3,4,5),(6,7,8),(0,3,6),(1,4,7),(2,5,8),(0,4,8),(2,4,6))
+    for each in win_coord:
+        if board[each[0]] == board[each[1]] == board[each[2]]:
+            return board[each[0]]
+    return False   
+
+def main(board):
+    counter = 0
+    win = False
+    X='X'
+    O='O'
+    while not win:
+        draw_board(board)
+        if counter % 2 == 0:
+            take_input(X)
+        else:
+            take_input(O)
+        counter += 1
+        if counter > 4:
+            tmp = check_win(board)
+            if tmp:
+                print(tmp, "выиграл!")
+                win = True
+                break
+        if counter == 9:
+            print("Ничья!")
+            break
+    draw_board(board)
+
+main(board)
